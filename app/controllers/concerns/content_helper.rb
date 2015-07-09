048859b8824ffba14d1805dd159be6444cee1ceb
timkellogg <tim.kellogg@gmail.com>
2015-07-09 00:34:04 -0700

    prep switch to devise controllers

---
+++ app/controllers/concerns/content_helper.rb
@@ -0,0 +1,18 @@
+# Need to figure out error of resources not found 
+# http://stackoverflow.com/questions/22614604/undefined-local-variable-or-method-resource-for-for-simple-form
+# http://stackoverflow.com/questions/22879222/do-i-need-a-users-controller-when-using-devise-in-rails
+
+
+# module ContentHelper
+#   def resource_name
+#     :user
+#   end
+
+#   def resource
+#     @resource ||= User.new
+#   end
+
+#   def devise_mapping
+#     @devise_mapping ||= Devise.mappings[:user]
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 	root :to => "posts#index" 
 
-  devise_for :users
-  
-  resources :users 
+  devise_for :users 
+  resources :users
   resources :posts do
   	resources :comments
 	end
